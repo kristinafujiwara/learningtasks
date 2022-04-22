7eda0dbd37a5c8dfa824137a02bba9824595004c
kristinafujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2022-04-22 16:30:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 		<li> <a href = "index.php"> HOME </a> </li>
 		<li> <a href = "/LT2/index.html"> LEARNING TASK 2 </a> </li>
 		<li> <a href = "/LT3/index.html"> LEARNING TASK 3 </a> </li>
-		<li> <a href = "/CLE1MT/index.html"> CLE Midterm Exam </a> </li>
 		<li> <a href = "/LT4/index.html"> LEARNING TASK 4 </a> </li>
 		<li> <a href = "/LT4/index.html"> LEARNING TASK 5 </a> </li>
+		<li> <a href = "/CLE1MT/index.html"> CLE Midterm Exam </a> </li>
 	</ul>
 	</body>
 </html>
