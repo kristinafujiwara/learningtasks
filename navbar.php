87a86a06483dacda2dffb89b61247fe76bbd4a1d
kristinafujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2022-05-18 13:16:41 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+  <title> Navigation Bar </title>
+  <meta charset = "utf-8">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1">
+  <link rel = "stylesheet" href = "stylesheet.css">
+  <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class = "navbar navbar-default">
+    <div class = "container-fluid">
+      <div class = "navbar-header">
+      </div>
+      <ul class = "nav navbar-nav">
+        <li class = "active"><a href = "index.php"> HOME </a></li>
+        <li><a href = "works.php"> WORKS </a></li>
+        <li><a href = "about.php"> ABOUT </a></li>
+      </ul>
+</nav>
+    </ul>
+  </div>
+</nav>
+  
+</body>
+</html>
